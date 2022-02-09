60daea976223257a6df7eea5db5bf438663813bf
Sayandip Halder <sayandiph4@gmail.com>
2022-02-10 02:01:48 +0530

    remove unnecessary files, use i18next to switch languages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import i18next from 'i18next';
+import { useTranslation, initReactI18next } from "react-i18next";
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'hin', 'ben'],
+    fallbackLng: 'en',
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: { useSuspense: false }
+  })
+
+function App() {
+  const { t } = useTranslation();
+
+  return <>
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+      <button onClick={() => i18next.changeLanguage('hin')}>Click here for Hindi</button>
+      <button onClick={() => i18next.changeLanguage('ben')}>Click here for Bengali</button>
+      <button onClick={() => i18next.changeLanguage('en')}>Click here for English</button>
+    </div>
+    <h2>{t('welcome_text')}</h2>
+  </>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +42,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
